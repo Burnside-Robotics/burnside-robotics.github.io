e7fdcea797d5aa494584a56a57ffce511556e35c
professoralex13 <alex@densem.net>
2020-11-19 22:17:14 +1300

    Delete OnImageOver.js

--- Front/Js/OnImageOver.js
+++
@@ -1,9 +0,0 @@
-var images = document.getElementsByClassName("RobotOrAward");
-for (var i = 0; i < images.length; i++) {
-    images[i].addEventListener("mouseover", function(e) {
-        document.getElementById(event.target.id + "Text").style.opacity = "1";
-    });
-    images[i].addEventListener("mouseleave", function(e) {
-        document.getElementById(event.target.id + "Text").style.opacity = "0.7";
-    });
-}
