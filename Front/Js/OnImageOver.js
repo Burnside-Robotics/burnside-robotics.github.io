5d5118d8d3442f0a2262de93a6489ec0a4abc376
professoralex13 <alex@massivedev.net>
2020-11-20 15:20:14 +1300

    Added Text animation to robots

--- Front/Js/OnImageOver.js
+++ Front/Js/OnImageOver.js
@@ -1,9 +1,12 @@
-var images = document.getElementsByClassName("RobotOrAward");
+var images = document.getElementsByClassName("RobotContainer");
+console.log(images.length);
 for (var i = 0; i < images.length; i++) {
     images[i].addEventListener("mouseover", function(e) {
-        document.getElementById(event.target.id + "Text").style.opacity = "1";
-    });
+        document.getElementById(e.target.id + "Text").style.bottom = "0";
+        document.getElementById(e.target.id + "Text").style.opacity = "1";
+    }); 
     images[i].addEventListener("mouseleave", function(e) {
-        document.getElementById(event.target.id + "Text").style.opacity = "0.7";
+        document.getElementById(e.target.id + "Text").style.bottom = "-10%";
+        document.getElementById(e.target.id + "Text").style.opacity = "0";
     });
 }
