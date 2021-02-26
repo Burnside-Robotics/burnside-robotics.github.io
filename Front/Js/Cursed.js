b54acd58552f3343b1382e64ee8e573bc983ed22
professoralex13 <alex@massivedev.net>
2021-02-26 18:37:40 +1300

    Cursed

--- Front/Js/Cursed.js
+++ Front/Js/Cursed.js
@@ -1,3 +1,4 @@
 document.getElementById("Cursed").onclick = function(){
     document.getElementById("MainPhoto").style.backgroundImage = "url('https://lh3.googleusercontent.com/-wQZB0lYoczY/X8AcS24ugfI/AAAAAAAABs4/wukTvMAS43UjAyXFx4gcITwabtLvm-UpACK8BGAsYHg/s0/AirportPhoto.png')";
-};
+    console.log("cursed time");
+};  
