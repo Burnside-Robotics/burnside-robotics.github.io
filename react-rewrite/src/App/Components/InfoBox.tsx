bc97fc7f835dd02b1be31e5c52863efbb04d42e0
Alex Cutforth <alex@densem.net>
2021-04-17 15:06:30 +1200

    started info box

---
+++ react-rewrite/src/App/Components/InfoBox.tsx
@@ -0,0 +1,40 @@
+import './InfoBox.scss'
+
+type infoProps = {
+    content: string[],
+    titles: string[]
+}
+
+export const InfoBox = (props: infoProps) =>
+{
+    if(props.content.length == 1) {
+        return (
+            <div className="info-box">
+                <h2>{props.titles[0]}</h2>
+                <p>
+                    {props.content[0]}
+                </p>
+            </div>
+        );
+    }
+    else
+    {
+        return (
+            <div className="info-box">
+                <div className='info-box-section'>
+                    <h2>{props.titles[0]}</h2>
+                    <p>
+                        {props.content[0]}
+                    </p>
+                </div>
+                <div className="info-box-splitter"/>
+                <div className='info-box-section'>
+                    <h2>{props.titles[1]}</h2>
+                    <p>
+                        {props.content[1]}
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
--- react-rewrite/src/App/Sections/About.tsx
+++ react-rewrite/src/App/Sections/About.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './About.scss'
+import { InfoBox } from '../Components/InfoBox'
+
 export const About = () =>
 {
     return(
         <section id="AboutSection">
-
+            <InfoBox content={["Burnside Robotics Club is a team of students attending Burnside High School that love to build robots. We have competed in multiple national Vex Robotics competitions and have qualified for the world competition. Our members are enthusiastic in the areas of STEM (Science, Technology, Engineering, and Maths) and enjoy competing with their creations.", "Burnside robotics offers a range of vital skills such as engineering, programming, teamwork, task management, and much more. These skills help students understand the world around them and help them build up skills in a safe, fun environment. "]} titles={["About Us", "What we Offer"]}/>
         </section>
     );
 }
