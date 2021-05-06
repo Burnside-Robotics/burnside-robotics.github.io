cf2e02b37979005f0c2185b95836571fbb701912
Alex Cutforth <alex@densem.net>
2021-05-06 17:43:39 +1200

    Remove Empty Gallery

--- react-rewrite/src/App/App.tsx
+++ react-rewrite/src/App/App.tsx
@@ -32,7 +32,7 @@ function App() {
             <div className="circuit-background-blur">
                 <section id="AboutSection">
                     <InfoBox content={["Burnside Robotics Club is a team of students attending Burnside High School that love to build robots. We have competed in multiple national Vex Robotics competitions and have qualified for the world competition. Our members are enthusiastic in the areas of STEM (Science, Technology, Engineering, and Maths) and enjoy competing with their creations.", "Burnside robotics offers a range of vital skills such as engineering, programming, teamwork, task management, and much more. These skills help students understand the world around them and help them build up skills in a safe, fun environment. "]} titles={["ABOUT US", "WHAT WE OFFER"]}/>
-                    <Gallery images={[]}></Gallery>
+                    {/*<Gallery images={[]}></Gallery>*/}
                 </section>
                 <SectionBanner src={AwardsTitle}/>
                 <section style={{paddingTop: "3%"}} id="AwardsSection">
