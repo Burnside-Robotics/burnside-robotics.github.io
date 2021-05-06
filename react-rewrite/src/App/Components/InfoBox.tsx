a2b958aaa0f934efe51ac6e9fbcc8fb0af43d028
Alex Cutforth <alex@densem.net>
2021-05-06 17:42:27 +1200

    Added Sponsors and Footer

--- react-rewrite/src/App/Components/InfoBox.tsx
+++ react-rewrite/src/App/Components/InfoBox.tsx
@@ -2,41 +2,61 @@ import './InfoBox.scss'
 
 type infoProps = {
     content: string[],
-    titles: string[]
+    titles: string[],
+    image?: string
 }
 
-export const InfoBox = (props: infoProps) =>
-{
-    if(props.content.length == 1) {
-        return (
-            <div style={{gridTemplateColumns: "100%"}} className="info-box">
+export const InfoBox = (props: infoProps) => {
+    if (props.image)
+    {
+        return(
+            <div style={{gridTemplateColumns: "45% auto 20%"}} className="info-box">
                 <div className='info-box-section'>
-                <h2>{props.titles[0]}</h2>
-                <p>
-                    {props.content[0]}
-                </p>
+                    <h2 style={{textAlign: "left"}}>{props.titles[0]}</h2>
+                    <p style={{textAlign: "left"}}>
+                        {props.content[0]}
+                    </p>
                 </div>
+                {/*Empty Div for column spacing*/}
+                <div></div>
+                <img src={props.image} alt="TaitLogo" />
             </div>
         );
     }
     else
     {
-        return (
-            <div className="info-box">
-                <div className='info-box-section'>
-                    <h2>{props.titles[0]}</h2>
-                    <p>
-                        {props.content[0]}
-                    </p>
+        if(props.content.length == 1) {
+            return (
+                <div style={{gridTemplateColumns: "100%"}} className="info-box">
+                    <div className='info-box-section'>
+                        <h2>{props.titles[0]}</h2>
+                        <p>
+                            {props.content[0]}
+                        </p>
+                    </div>
                 </div>
-                <div className="info-box-splitter"/>
-                <div className='info-box-section'>
-                    <h2>{props.titles[1]}</h2>
-                    <p>
-                        {props.content[1]}
-                    </p>
+            );
+        }
+        else
+        {
+            return (
+                <div className="info-box">
+                    <div className='info-box-section'>
+                        <h2>{props.titles[0]}</h2>
+                        <p>
+                            {props.content[0]}
+                        </p>
+                    </div>
+                    <div className="info-box-splitter"/>
+                    <div className='info-box-section'>
+                        <h2>{props.titles[1]}</h2>
+                        <p>
+                            {props.content[1]}
+                        </p>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
+
 }
