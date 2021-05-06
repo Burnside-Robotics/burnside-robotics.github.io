6d7e7c19e4182e9ca17d26fa133b24c0115a56a0
Alex Cutforth <alex@densem.net>
2021-05-06 17:34:35 +1200

    Updates

--- parallax.js
+++ parallax.js
@@ -1,14 +1,17 @@
 const background_image_parallax = function(object, multiplier){
     multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
     multiplier = 1 - multiplier;
-    object.style.backgroundAttachment = "fixed"
-    window.onscroll = function(){
+    object.forEach((item, index) => {
+        item.style.backgroundAttachment = "fixed"
+    });
+    window.onscroll =function(){
         const from_top = window.pageYOffset;
-        object.style.backgroundPositionY = (multiplier * -from_top) + 'px';
+        object.forEach((item, index) => {
+            item.style.backgroundPositionY = (multiplier * -from_top) + 'px';
+        });
     };
 };
 
 //Just pass the jQuery object
 //optional second value for speed
-background_image_parallax(document.getElementsByClassName("parallax-window")[0], 0.5);
-// background_image_parallax(document.getElementsByClassName("circuit-background")[0], 0.5);
+background_image_parallax([document.getElementsByClassName("circuit-background")[0], document.getElementsByClassName("parallax-window")[0]], 0.5);
