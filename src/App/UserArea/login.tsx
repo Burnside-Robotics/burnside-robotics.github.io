3a526d5be52b0a3ba696386881cfad256d78f07e
Alex Cutforth <alex@densem.net>
2021-05-19 21:34:19 +1200

    Added Basic Sign in

--- src/App/UserArea/login.tsx
+++ src/App/UserArea/login.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HeaderLogo from '../Assets/Photos/HeaderLogo.png'
+import './style.scss';
+import firebase from "firebase";
+import {useHistory} from 'react-router-dom';
+import {useSignedIn} from "../useSignedIn";
 export const Login = () =>
 {
+    const [email, setEmail] = useState(String);
+    const [password, setPassword] = useState(String);
+    const history = useHistory();
+    const isSignedIn = useSignedIn();
+    function SignIn()
+    {
+        firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) =>
+        {
+            console.log('Sign in Successful');
+        }).catch((error) =>
+        {
+            console.log('Sign in Failed');
+        });
+    }
+    if(isSignedIn)
+        history.push('/user')
     return(
-        <div>
-            <div>
-                <img className='centre-horizontally' src={HeaderLogo} alt='robotics-photo'/>
-            </div>
+        <div className='login-container'>
+            <img className='' src={HeaderLogo} alt='robotics-photo'/>
+            <input type="text" name="email" placeholder='Email' value={email} onChange={(value) => setEmail(value.target.value)}/>
+            <input type="text" name="password" placeholder='Password' value={password} onChange={(value) => setPassword(value.target.value)}/>
+            <button onClick={() => SignIn()} >Sign In</button>
         </div>
     );
 }
---
+++ src/App/useSignedIn.tsx
@@ -0,0 +1,13 @@
+import {useEffect, useState} from "react";
+import firebase from "firebase";
+export const useSignedIn = () =>
+{
+    const [signedIn, setSignedIn] = useState(false);
+    useEffect(() =>{
+        firebase.auth().onAuthStateChanged(user =>
+        {
+           setSignedIn(user != null);
+        });
+    }, [])
+    return(signedIn);
+}
