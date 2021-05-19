7b00306b13d30aa038d90117e373636e350f74bd
Alex Cutforth <alex@densem.net>
2021-05-19 15:07:36 +1200

    Add firebase init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App.tsx';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCMl-G95hqOYmfgJPgRCbATuE6r5s-AT9s",
+    authDomain: "burnside-robotics-website.firebaseapp.com",
+    projectId: "burnside-robotics-website",
+    storageBucket: "burnside-robotics-website.appspot.com",
+    messagingSenderId: "32022669627",
+    appId: "1:32022669627:web:4f620f64d8e474ea222f1e",
+    measurementId: "G-QQWBB5QGHY"
+});
 
 ReactDOM.render(
   <React.StrictMode>
