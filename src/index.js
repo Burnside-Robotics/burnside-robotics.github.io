13bddc89766ae8821decfc243cdc8b8f3cf0ba33
Alex Cutforth <alex@densem.net>
2021-05-19 15:55:12 +1200

    Start Routing System

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App.tsx';
+import {App} from './App/App.tsx';
 import reportWebVitals from './reportWebVitals';
 import firebase from "firebase";
 
