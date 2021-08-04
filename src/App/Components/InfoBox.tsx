0f9f0d430764805f68a680b077611d8300d5ce5e
Alex Cutforth <alex@densem.net>
2021-08-04 14:39:43 +1200

    fix info box sizing bugs and

--- src/App/Components/InfoBox.tsx
+++ src/App/Components/InfoBox.tsx
@@ -10,15 +10,12 @@ export const InfoBox = (props: infoProps) => {
 	if (props.image) {
 		return (
 			<div
-				style={{ gridTemplateColumns: "45% auto 20%" }}
 				className="info-box"
 			>
-				<div className="info-box-section">
+				<div>
 					<h2 style={{ textAlign: "left" }}>{props.titles[0]}</h2>
 					<p style={{ textAlign: "left" }}>{props.content[0]}</p>
 				</div>
-				{/*Empty Div for column spacing*/}
-				<div></div>
 				<img src={props.image} alt="TaitLogo" />
 			</div>
 		);
@@ -26,10 +23,9 @@ export const InfoBox = (props: infoProps) => {
 		if (props.content.length === 1) {
 			return (
 				<div
-					style={{ gridTemplateColumns: "100%" }}
 					className="info-box"
 				>
-					<div className="info-box-section">
+					<div>
 						<h2>{props.titles[0]}</h2>
 						<p>{props.content[0]}</p>
 					</div>
